b9194160a83c5ec704e41b422eb6665ef5b9d8c9
Shovon96 <fakhruddinshovon@gmail.com>
2025-10-20 16:59:43 +0600

    doctor and admin routes created

--- healt-care-backend/src/app/modules/users/user.router.ts
+++ healt-care-backend/src/app/modules/users/user.router.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from "express";
 import { UserController } from "./user.controller";
 import { UserValidation } from "./user.validation";
 import { FileUploader } from "../../helper/fileUploader";
+import { UserRole } from "@prisma/client";
 
 
 const router = Router();
@@ -11,6 +12,27 @@ router.post('/create-patient',
     (req: Request, res: Response, next: NextFunction) => {
         req.body = UserValidation.createUserZodSchema.parse(JSON.parse(req.body.data));
         return UserController.createPatient(req, res, next)
+    }
+);
+
+router.post(
+    "/create-admin",
+    // auth(UserRole.ADMIN),
+    FileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = UserValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createAdmin(req, res, next)
+    }
+);
+
+router.post(
+    "/create-doctor",
+    // auth(UserRole.ADMIN),
+    FileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log(JSON.parse(req.body.data))
+        req.body = UserValidation.createDoctorValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createDoctor(req, res, next)
     }
 );
 
