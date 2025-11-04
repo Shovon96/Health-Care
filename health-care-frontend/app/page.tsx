cad07a581fc24728965a83d2731a0d0d2352e779
Shovon96 <fakhruddinshovon@gmail.com>
2025-11-04 18:56:30 +0600

    frontend setup with nextjs, shdcn installion done

---
+++ health-care-frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+      <p>Health care frontend</p>
+      <Button>Honme</Button>
+   </div>
+  );
+}
