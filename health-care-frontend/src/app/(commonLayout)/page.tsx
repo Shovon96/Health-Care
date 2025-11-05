509e75c09c1bfa4237cd18cd0927ffb58c0508b0
Shovon96 <fakhruddinshovon@gmail.com>
2025-11-05 20:18:02 +0600

    doctor specialist added

--- health-care-frontend/src/app/(commonLayout)/page.tsx
+++ health-care-frontend/src/app/(commonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/src/components/modules/home/Hero";
+import Specialities from "@/src/components/modules/home/Specialities";
 
 
 export default function HomePage() {
   return (
     <div>
         <Hero />
+        <Specialities />
     </div>
   )
 }
