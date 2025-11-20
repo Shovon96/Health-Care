d0be41d70f7db81339bc64396c61b3f00ade903b
Shovon96 <fakhruddinshovon@gmail.com>
2025-11-20 18:27:37 +0600

    create server fetch for reuseable fetch function

---
+++ health-care-frontend/src/lib/server-fetch.ts
@@ -0,0 +1,38 @@
+import { getCookie } from "../components/modules/auth/tokenHandlers";
+
+const backendURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/v1";
+
+const serverFetchHelper = async (endpoint: string, options: RequestInit): Promise<Response> => {
+
+    const { headers, ...restOptions } = options;
+    const accessToken = await getCookie("accessToken")
+
+    const response = await fetch(`${backendURL}${endpoint}`, {
+        headers: {
+            ...headers,
+            Cookie: accessToken ? `accessToken=${accessToken}` : "",
+        },
+        ...restOptions,
+    });
+
+    return response;
+}
+
+
+export const serverFetch = {
+    get: async (endpoint: string, options: RequestInit = {}): Promise<Response> => {
+        return serverFetchHelper(endpoint, { method: 'GET', ...options });
+    },
+    post: async (endpoint: string, options: RequestInit = {}): Promise<Response> => {
+        return serverFetchHelper(endpoint, { method: 'POST', ...options });
+    },
+    put: async (endpoint: string, options: RequestInit = {}): Promise<Response> => {
+        return serverFetchHelper(endpoint, { method: 'PUT', ...options });
+    },
+    patch: async (endpoint: string, options: RequestInit = {}): Promise<Response> => {
+        return serverFetchHelper(endpoint, { method: 'PATCH', ...options });
+    },
+    delete: async (endpoint: string, options: RequestInit = {}): Promise<Response> => {
+        return serverFetchHelper(endpoint, { method: 'DELETE', ...options });
+    },
+}
