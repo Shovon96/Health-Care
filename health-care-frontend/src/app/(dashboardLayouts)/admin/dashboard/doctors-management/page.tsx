b65cd59263f69682aeae447c062f5aaedce95d7d
Shovon96 <fakhruddinshovon@gmail.com>
2025-11-26 22:06:59 +0600

    doctors table search fuctionality completed

--- health-care-frontend/src/app/(dashboardLayouts)/admin/dashboard/doctors-management/page.tsx
+++ health-care-frontend/src/app/(dashboardLayouts)/admin/dashboard/doctors-management/page.tsx
@@ -6,19 +6,24 @@ import RefreshButton from "@/src/components/shared/RefreshButton";
 import SearchFilter from "@/src/components/shared/SearchFilter";
 import SelectFilter from "@/src/components/shared/SelectFilter";
 import { TableSkeleton } from "@/src/components/shared/TableSkeleton";
+import { queryStringFormatter } from "@/src/lib/formatters";
 import { ISpecialty } from "@/src/types/specialities.interface";
 import { Suspense } from "react";
 
-export default async function AdminDoctorsManagementPage() {
+export default async function AdminDoctorsManagementPage({ searchParams }: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
 
+    const searchParamsObj = await searchParams;
+    const queryString = queryStringFormatter(searchParamsObj);
     const specialistResult = await getSpeciality();
-    const doctorsResult = await getAllDoctors()
+    const doctorsResult = await getAllDoctors(queryString);
 
     return (
         <div className="space-y-6">
             <DoctorManagementHeader specialities={specialistResult.data} />
             <div className="flex gap-2">
-                <SearchFilter placeholder="Search doctors..." />
+                <SearchFilter paramName="searchTerm" placeholder="Search doctors..." />
                 <SelectFilter
                     paramName="specialist"
                     options={specialistResult.data.map((specialty: ISpecialty) => ({
